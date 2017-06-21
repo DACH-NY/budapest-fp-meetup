05e465918988f681cfdbd64663c3cdc08eca089d
Gyorgy Farkas <gyorgy.farkas@digitalasset.com>
2017-06-21 12:59:01 +0200

    added readline

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -1,28 +1,26 @@
 module Main where
 
-
 import System.IO
+import System.Console.Readline
 import Control.Monad
 import Parser
 import Eval
-
-flushStr :: String -> IO ()
-flushStr str = putStr str >> hFlush stdout
-
-readPrompt :: String -> IO String
-readPrompt prompt = flushStr prompt >> getLine
-
-until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
-until_ pred prompt action = do 
-   result <- prompt
-   if pred result 
-      then return ()
-      else action result >> until_ pred prompt action
            
+readEvalPrintLoop :: IO ()
+readEvalPrintLoop = do
+   maybeLine <- readline "CEK>>> "
+   case maybeLine of
+    Nothing     -> return () -- EOF / control-d
+    Just "exit" -> return ()
+    Just line -> do addHistory line
+                    putStrLn . show  . run $ line
+                    readEvalPrintLoop
+
+    where run input = (tryParseExpr input) >>= evaluate
+
 
 main :: IO ()
 main = do
   _ <- putStrLn "Welcome to cek-scheme"
-  until_ (== ":quit") (readPrompt ">>> ")  (putStrLn . show  . run)
-  where run input = (tryParseExpr input) >>= evaluate
-          
+  readEvalPrintLoop
+
